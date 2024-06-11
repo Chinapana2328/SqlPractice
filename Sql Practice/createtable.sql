715dc653770b6732734664131742005bc47a563a
unknown <saraswathireddychinapana@gmail.com>
2024-06-11 22:16:07 +0530

    intial commit

--- Sql Practice/createtable.sql
+++ Sql Practice/createtable.sql
@@ -1,3 +1,20 @@
 CREATE TABLE university
 (emp_id,emp_name,emp_age,Department
  );
+
+SELECT * FROM employees;
+
+
+SELECT * FROM employees
+WHERE department = 'Sales';
+
+SELECT * FROM employees
+ORDER BY last_name ASC;
+
+SELECT AVG(salary) AS average_salary
+FROM employees;
+
+SELECT department, COUNT(*) AS employee_count
+FROM employees
+GROUP BY department;
+
